c013d93eade56b01379ff0cd0b7cffff018f3c63
Shirsendu Das <shirsendudas49@gmail.com>
2026-02-21 12:33:10 +0530

    CI fix

--- src/app/api/novu/route.ts
+++ src/app/api/novu/route.ts
@@ -1,4 +1,6 @@
 import { serve } from '@novu/framework/next';
 import { allWorkflows } from '@/lib/novu-workflows';
 
+export const dynamic = 'force-dynamic';
+
 export const { GET, POST, OPTIONS } = serve({ workflows: allWorkflows });
